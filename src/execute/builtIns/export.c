145933605044b039fb3e086deba14a66f7dfa4c6
sshahary <145033155+sshahary@users.noreply.github.com>
2024-05-02 16:36:23 +0200

    fix export

--- src/execute/builtIns/export.c
+++ src/execute/builtIns/export.c
@@ -20,7 +20,7 @@ static void	printexport(t_mini *mini)
 	i = 0;
 	while (mini->env[i])
 	{
-		// if (mini->env[i] ==  mini)
+		ft_putstr_fd("declare -x ", STDIN_FILENO);
 		ft_putstr_fd(mini->env[i], STDIN_FILENO);
 		write(STDOUT_FILENO, "\n", 1);
 		i++;
@@ -33,20 +33,38 @@ static void	addexport(char *path, char **new, int i)
 	new[i + 1] = NULL;
 }
 
+// Get the index of first occurrance of character c
+// Return NULL if not found
+static int	str_index_of(char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (i);
+		++i;
+	}
+	return (-1);
+}
+
 int	checkexport(char *path, char ***env)
 {
 	int		i;
 	char	**new;
-
 	if (path[0] == '=' || path[0] == '\0' || ft_strlen(path) == 1)
 		return (-1);
 	i = -1;
 	while ((*env)[++i] != NULL)
 	{
-		if (!(ft_strncmp((*env)[i], path, ft_strlen(path))))
+		// printf("Checking env[%d]: %s\n", i, (*env)[i]);
+		if (!ft_strncmp((*env)[i], path, str_index_of(path, '=')))
 		{
-			// printf("env: %s\t path: %s\n", *env[i], path);
+			// printf("Found matching path: %s\n", (*env)[i]);
+			// printf("Replacing with: %s\n", path);
 			(*env)[i] = ft_strdup(path);
+			// printf("Updated env[%d]: %s\n", i, (*env)[i]);
 			return (1);
 		}
 	}
@@ -59,7 +77,6 @@ int	checkexport(char *path, char ***env)
 	*env = new;
 	return (1);
 }
-
 
 int		isvalidnum(char *str)
 {
@@ -75,6 +92,24 @@ int		isvalidnum(char *str)
 	return (1);
 }
 
+// static void updateexport(char *name, char *value, char **env)
+// {
+// 	int i = 0;
+// 	char *variable = ft_strjoin(name, "=");
+// 	char *new_entry = ft_strjoin(variable, value);
+// 	free(variable);
+
+// 	while (env[i] != NULL)
+// 	{
+// 		if (ft_strncmp(env[i], name, ft_strlen(name)) == 0)
+// 		{
+// 			free(env[i]);
+// 			env[i] = new_entry;
+// 			return;
+// 		}
+// 		i++;
+// 	}
+// }
 // void	export(char **cmds, t_mini *mini)
 void	export(t_mini *mini)
 {
@@ -90,7 +125,7 @@ void	export(t_mini *mini)
 	else
 	{
 		remove_char(mini->cmds->args[1], '\'');
-		while (mini->cmds->args[i++])
+		while (mini->cmds->args[++i])
 		{
 			if (isvalidnum(ft_strtok(tmp[i], '=')) == 0)
 			{
@@ -104,34 +139,3 @@ void	export(t_mini *mini)
 	if (res != 1)
 		mini->exit_code = 1;
 }
-
-// void	export(t_mini *mini)
-// {
-// 	int		i;
-// 	int		res;
-// 	char 	**tmp;
-
-// 	res = 0;
-// 	i = 0;
-// 	tmp = mini->env;
-// 	if (mini->preflag == 1)
-// 		return;
-
-// 	if (ft_dstrlen(mini->env) == 1) 
-// 		printexport(mini);
-// 	else {
-// 		while (mini->env[++i])
-// 		{
-// 			if (isvalidnum(ft_strtok(tmp[i], '=')) == 0)
-// 			{
-// 				ft_iderr("export", tmp[i]);
-// 				mini->exit_code = 1;
-// 			}
-// 			remove_char(mini->env[i], '$');
-// 			res = checkexport(mini->env[i], &(mini->env));
-// 		}
-// 	}
-
-// 	if (res != 1)
-// 		mini->exit_code = 1;
-// }
